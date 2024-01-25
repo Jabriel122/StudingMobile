df4c227c2c060ea6c4633d2e06fa0bd54ff0456b
Gabriel <gbielmarch@gmail.com>
2024-01-25 10:58:45 -0300

    Pensar em alguns bagui

---
+++ Mobile/Exercicio-1Project/ExerciseOne/App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("./src/assets/NaoSiaDAgua.png")} style={styles.image}></Image>
+      <Text style={styles.login}>LOGIN</Text>
+      <View style={styles.containerEmail} >
+        <Text style={styles.email}>Email</Text>
+        <TextInput />
+      </View>
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#6D7FF9',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerEmail: {
+    backgroundColor: '#6D7FF9',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  image: {
+    width: '100%'
+  },
+  login: {
+    color: 'white',
+    width: '70%',
+    textAlign: 'center',
+    fontWeight: '600',
+    fontSize: 30,
+    margin: 10
+  },
+  email: {
+    color: 'white',
+  }
+});
